04e4e952b4b16a62df731c42644a1dac588cf625
Arka <arkajyotiadhikary15@gmail.com>
2020-10-13 14:17:09 +0530

    day2 done

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+        // float 
+        public float bulletSpeed;
+        public float bulletDestroyTime;
+        float time;
+        //  rigidbody
+        Rigidbody rb;
+
+        private void Start() {
+            rb = GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward*bulletSpeed,ForceMode.Impulse);
+
+            if(time >= bulletDestroyTime)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                time += Time.deltaTime;
+            }
+        }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // float 
+    public float bulletSpawnTimer;
+    float time;
+    // transform
+    public Transform muzzle;
+
+    // gameobject
+    public GameObject bullet;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(time>bulletSpawnTimer)
+        {
+            time = bulletSpawnTimer;
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+
+        if(Input.GetMouseButtonDown(0) && time == bulletSpawnTimer)
+        {
+            Instantiate(bullet,muzzle.position,muzzle.rotation,null);
+        }        
+       
+    }
+}
