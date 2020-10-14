953f3d83a2fbd08ce84ed83bb3f223a76c7972ec
Arka <arkajyotiadhikary15@gmail.com>
2020-10-14 18:52:12 +0530

    day3 done

---
+++ Assets/Scripts/Box_Destroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Box_Destroy : MonoBehaviour
+{
+    public ParticleSystem destroyPartical;
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.collider.CompareTag("Bullet"))
+        {
+            StartCoroutine(destroy());
+        }
+    }
+
+    IEnumerator destroy()
+    {
+        GetComponent<Collider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled=false;
+        destroyPartical.Play();
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
